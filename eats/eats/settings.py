1668f42dc2278e872987c9dbb26c3a91417e4294
d3n1AL <dlx3ud@virginia.edu>
2022-03-13 17:53:19 -0400

    Configure settings.py to use django all-auth

--- eats/eats/settings.py
+++ eats/eats/settings.py
@@ -127,3 +127,27 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
+SITE_ID = 2
+
+# These URLs are where the users are redirected to after login and logout
+# Right now both are set to the base url of the website
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
