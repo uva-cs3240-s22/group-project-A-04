7ee01dcd16e61b98ec9489b97d7f486a642e8a92
Samuel Ahn <89561846+sya5jv@users.noreply.github.com>
2022-03-26 12:01:19 -0400

    Updated DATABASE to use postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,6 +86,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# Configured for postgreSQL for Heroku
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
