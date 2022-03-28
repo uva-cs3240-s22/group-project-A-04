7a3def05a2a40087ab15bdc384691d529c103077
d3n1AL <dlx3ud@virginia.edu>
2022-03-28 13:54:14 -0400

    Add multiple ingredient form

--- recipes/views.py
+++ recipes/views.py
@@ -8,12 +8,17 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+# model form for query sets
+from django.forms.models import inlineformset_factory
+# cited from this youtube tutorial:
+# https://youtu.be/6wHx-X1tEiY
+
 # Importing models from current directory
-from .models import Recipe
+from .models import Recipe, Ingredient
 from .forms import RecipeForm, IngredientForm
 
 # make login required before any of these views can be accessed
-# taken from this youtube video: https://youtu.be/PICYTJqj__o
+# taken from this YouTube video: https://youtu.be/PICYTJqj__o
 
 
 class RecipeIndex(ListView):
@@ -27,6 +32,7 @@ class RecipeIndex(ListView):
 class RecipeDetail(DetailView):
     model = Recipe
     template_name = 'recipes/detail.html'
+
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
@@ -61,29 +67,35 @@ def recipe_create_view(request):
 
     # make a form for recipes and ingredients
     recipe_form = RecipeForm(request.POST or None)
-    ingredient_form = IngredientForm(request.POST or None)
+    # ingredient_form = IngredientForm(request.POST or None)
+
+    # make instance of Formset
+    IngredientInlineFormset = inlineformset_factory(Recipe, Ingredient,
+                                                    fields=('ingredient_name', 'quantity'))
+    ingredient_set = IngredientInlineFormset(request.POST or None)
 
     # for loading html
     context = {
         "recipe_form": recipe_form,
-        "ingredient_form": ingredient_form,
+        # "ingredient_form": ingredient_form,
+        "ingredient_set": ingredient_set,
     }
 
     # check that the form is valid, if so, submit
-    if all([recipe_form.is_valid(), ingredient_form.is_valid()]):
+    if all([recipe_form.is_valid(), ingredient_set.is_valid()]):
         recipe = recipe_form.save(commit=False)     # commit = False does not add to DB
         recipe.author = request.user
-
-        ingredient = ingredient_form.save(commit=False)
-        ingredient.recipe = recipe
-
         recipe.save()
-        ingredient.save()
+
+        for ingredient_form in ingredient_set:
+            ingredient = ingredient_form.save(commit=False)
+            ingredient.recipe = recipe
+            ingredient.save()
 
         # confirmation message
         context['message'] = 'Recipe saved!'
 
         return redirect(reverse('recipes:index'))
 
-    # otherwise redirect to original form
+    # otherwise, redirect to original form
     return render(request, template, context)
