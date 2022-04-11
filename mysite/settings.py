239c851233a259fd9794f6fbe27ac9a2104106d6
d3n1AL <dlx3ud@virginia.edu>
2022-04-10 23:12:48 -0400

    Add media storage backend

--- mysite/settings.py
+++ mysite/settings.py
@@ -156,13 +156,17 @@ AWS_SECRET_ACCESS_KEY = 'aztNQkzYZwwBj1XFFan4u1Fp1BLYop1lxho84OUz'
 AWS_STORAGE_BUCKET_NAME = 'group-project-a-04'
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-AWS_LOCATION = 'static'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STATIC_LOCATION = 'static'
+STATICFILES_STORAGE = 'mysite.storage_backends.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+
+AWS_MEDIA_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mysite/static'),
